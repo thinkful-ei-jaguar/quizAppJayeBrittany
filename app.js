b54abe9c1604203b520754d3e1bd31bb8a837165
Brittany <brittjsauce@yahoo.com>
2019-12-06 13:32:58 -0600

    save

--- app.js
+++ app.js
@@ -65,7 +65,10 @@ const store = {
   questionNumber: 0,
   score: 0,
   quizStarted: false,
-  answered: false
+  answered: false,
+  currentCorrectAnswer: null,
+  correct: null,
+  answerChoice = null
 };
 
 /**
@@ -88,93 +91,80 @@ function generateStarterHTML() {
 }
 
 function generateResultsPage() {
-  return ` <header>
+  return `<header>
   Results
 </header>
 <section>
 <p>Your Score:
 <span class="final-score">6</span>/10</p>
 </section>
-
 <button class="btn js-restart-btn">
 RESTART
 </button>`;
 }
 
-function generateQuestionHTML() {
+function generateAnswerChoiceHTML(answerChoice) {
+  let feedback = null;
+  if (store.answered === true) {
+    if (store.answerChoice === answerChoice) {
+      feedback = store.correct ? '<div class="correct">Correct!</div>' : '<div class="incorrect">Incorrect!</div>';
+    }
+  }
+  return `<div id="option-container-a">
+  <label for="a" />
+  <input name="answerChoice" value=${answerChoice} id="a" type="radio">${answerChoice}
+  ${feedback}
+</div>`;
+}
+
+function generateQuestionsHTML() {
   let q = store.questions.find(q => q.id === store.questionNumber);
+  store.currentCorrectAnswer = q.correctAnswer;
+
+  let btnContent = store.answered ? 'Next' : 'Submit'
 
   return `<form id="main-form" action="">
   <fieldset>
       <legend>
           ${q.question}
       </legend>
-      <div id="option-container-a">
-          <label for="a" />
-          <input name="answerChoice" value="a" id="a" type="radio">${q.answers[0]}
-          <div class="hidden">CORRECT</div>
-      </div>
-
-
-      <div id="option-container-b">
-          <label for="b" />
-          <input name="answerChoice" value="b" id="a" type="radio">${q.answers[1]}
-      </div>
-
-      <div id="option-container-c">
-          <label for="c" />
-          <input name="answerChoice" value="c" id="a" type="radio">${q.answers[2]}
-      </div>
-
-      <div id="option-container-c">
-          <label for="d" />
-          <input name="answerChoice" value="d" id="a" type="radio" />${q.answers[3]}
-      </div>
-
+      ${q.answers.map(a => generateAnswerChoiceHTML(a))}
       <button type="submit" value="submit" class="js-next-submit-btn">
-
+      ${btnContent}
       </button>
   </fieldset>
 </form>`;
 }
 
-function generateFeedbackHTML(c) {
-  if (c === 'correct') {
-    return `<div class="correct">CORRECT!</div>`;
-  } else {
-    return `<div class="incorrect">INCORRECT!</div>;`
-  }
-}
-
 function handleSubmitClick() {
   $('main').on('submit', '#main-form', e => {
     e.preventDefault();
-    let html = '';
+    console.log('user clicked submit button');
     let answerChoice = $('input[name=answerChoice]:checked').val();
+    console.log(answerChoice);
 
-    let question = store.find(q => q.id === store.questionNumber);
+    let question = store.questions.find(q => q.id === store.questionNumber);
 
     if (question.correctAnswer === answerChoice) {
-      html += generateFeedbackHTML('correct');
+      store.correct = true;
     } else {
-      html += generateFeedbackHTML('incorrect');
+      store.correct = false;
     }
-    // APPEND FEEDBACK DIV TO SELECTED ANSWER CHOICE
-    let targetedDiv =
-      render();
 
-  });
-
-}
-
-function handleNextClick() {
-  $('main').on('click', '.next-submit-btn', e => {
-    if ($(e.currentTarget.val() === 'next' && store.answered === true)) {
+    console.log($(e.currentTarget).find('button').text());
+    if ($(e.currentTarget).find('button').text().trim() === 'Submit') { // submit button pressed
+      store.answered = true;
+      let answerChoice = $('input[name=answerChoice]:checked').val();
+      store.answerChoice = answerChoice;
+    } else { //next button pressed
       store.questionNumber++;
       store.answered = false;
-      render();
+      store.correct = null;
     }
+
+    render();
   });
+
 }
 
 // When user clicks start button, set quiz started to true, update question number, re-render
@@ -193,7 +183,7 @@ function render() {
   if (store.questionNumber === 0) {
     html += generateStarterHTML();
   } else if (store.questionNumber > 0 && store.questionNumber <= 5) {
-    html += generateQuestionHTML();
+    html += generateQuestionsHTML();
   } else {
     html += generateResultsPage();
   }
@@ -206,6 +196,8 @@ function handleRestartClick() {
     store.questionNumber = 0;
     store.score = 0;
     store.quizStarted = false;
+    store.answered = false;
+    store.correct = null;
     render();
   });
 }
@@ -213,7 +205,6 @@ function handleRestartClick() {
 function handleQuizApp() {
   render();
   handleStartClick(); // event listeners 
-  handleNextClick();
   handleSubmitClick();
   handleRestartClick();
 }
